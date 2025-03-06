0a0b2760b101539ec7468724ef83e721dd8d708f
gaeunchoi <gaeun1884@naver.com>
2025-03-06 16:24:40 +0900

    Test: 2025-03-06 JS 코딩 기초 트레이닝

--- 최가은/JS_코딩 기초 트레이닝/문자열 붙여서 출력하기.js
+++ 최가은/JS_코딩 기초 트레이닝/문자열 붙여서 출력하기.js
@@ -1,3 +1,15 @@
-// 문자열 붙여서 출력하기
-str1, str2 = input().strip().split(' ')
-console.log(str1 + str2)
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    str1 = input[0];
+    str2 = input[1];
+    console.log(str1+str2)
+});
