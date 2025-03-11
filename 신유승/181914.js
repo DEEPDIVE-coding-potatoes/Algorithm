3bbc5459bd1703b1035e7b565c522386df4e4086
Yuseoung Shin <128285472+SinYusi@users.noreply.github.com>
2025-03-11 17:11:25 +0900

    Test: 신유승 20250311 문제풀이 (#25)
    * Test: 주사위 게임 3(181916) 풀이
    
    * Test: 181916 좋아요 많은 풀이 추가
    
    * Test: 글자 이어 붙여 문자열 만들기(181915) 풀이
    
    * Test: 9로 나눈 나머지(181914) 풀이
    
    * Test: 문자열 여러 번 뒤집기(181913) 풀이
    
    * Test: 배열 만들기 5(181912) 풀이
    
    * Test: 부분 문자열 이어 붙여 문자열 만들기(181911) 풀이

---
+++ 신유승/181914.js
@@ -0,0 +1,18 @@
+/**
+ * 음이 아닌 정수를 9로 나눈 나머지는 그 정수의 각 자리 숫자의 합을 9로 나눈 나머지와 같은 것이 알려져 있습니다.
+ * 이 사실을 이용하여 음이 아닌 정수가 문자열 number로 주어질 때, 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
+ * 제한사항
+ * 1 ≤ number의 길이 ≤ 100,000
+ * number의 원소는 숫자로만 이루어져 있습니다.
+ * number는 정수 0이 아니라면 숫자 '0'으로 시작하지 않습니다.
+ */
+
+function solution(number) {
+  const sum = number.split('').reduce((acc, cur) => {
+    return acc += Number(cur);
+  }, 0)
+  return sum % 9;
+}
+
+// 좋아요 많은 풀이
+// const solution=n=>BigInt(n)%9n
