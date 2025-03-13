d5edb9ce27263a390362d466105b5fcd15de5e14
Yusi <yusi4781@naver.com>
2025-03-13 16:15:50 +0900

    Test: 카운트 다운(181899) 풀이

---
+++ 신유승/181899.js
@@ -0,0 +1,16 @@
+/**
+ * 정수 start_num와 end_num가 주어질 때, 
+ * start_num에서 end_num까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+ * 
+ * 제한사항
+ * 0 ≤ end_num ≤ start_num ≤ 50
+ */
+
+function solution(start_num, end_num) {
+  const answer = [];
+  for (let i = start_num; i >= end_num; i--) answer.push(i);
+  return answer;
+}
+
+//좋아요 많은 풀이
+//const solution = (start, end) => Array(start-end+1).fill(start).map((v,i)=>v-i);
