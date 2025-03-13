bad128e3c89e8f0bb870111c461b27eb6b2a6a64
Yuseoung Shin <128285472+SinYusi@users.noreply.github.com>
2025-03-13 17:00:17 +0900

    Test: 2025-03-13 신유승 알고리즘 풀이 (#33)
    * Test: 글자 지우기(181900) 풀이
    
    * Test: 카운트 다운(181899) 풀이
    
    * Test: 가까운 1 찾기(181898) 풀이
    
    * Test: 리스트 자르기(181897) 풀이
    
    * Test: 첫 번째로 나오는 음수(181896) 풀이
    
    * Test: 배열 만들기 3(181895) 풀이
    
    * Test: 2의 영역(181894) 풀이
    
    * Test: 배열 조각하기(181893) 풀이
    
    * Test: n번째 원소부터(181892) 풀이
    
    * Test: 가까운 1 찾기 좋아요 많은 풀이 추가
    
    * Test: 리스트 자르기 풀이 수정
    
    * Test: 배열 조각하기 풀이 수정
    
    * Test: 순서 바꾸기(181891) 풀이

---
+++ 신유승/181895.js
@@ -0,0 +1,21 @@
+/**
+ * 정수 배열 arr와 2개의 구간이 담긴 배열 intervals가 주어집니다.
+ * intervals는 항상 [[a1, b1], [a2, b2]]의 꼴로 주어지며 각 구간은 닫힌 구간입니다. 
+ * 닫힌 구간은 양 끝값과 그 사이의 값을 모두 포함하는 구간을 의미합니다.
+ * 이때 배열 arr의 첫 번째 구간에 해당하는 배열과 두 번째 구간에 해당하는 배열을 앞뒤로 붙여 새로운 배열을 만들어 return 하는 solution 함수를 완성해 주세요.
+ * 
+ * 제한사항
+ * 1 ≤ arr의 길이 ≤ 100,000
+ * 1 ≤ arr의 원소 < 100
+ * 1 ≤ a1 ≤ b1 < arr의 길이
+ * 1 ≤ a2 ≤ b2 < arr의 길이
+ */
+
+const solution = (arr, intervals) => intervals.reduce((acc, [a, b]) => [...acc, ...arr.slice(a, b + 1)], [])
+
+//좋아요 많은 풀이
+// function solution(arr, intervals) {
+//   const [[a, b], [c, d]] = intervals;
+
+//   return [...arr.slice(a, b + 1), ...arr.slice(c, d + 1)];
+// }
