ce5c2e80d47c7ae5df20aca47ff8a697282ca4b4
권수영 <113653579+ksy21019@users.noreply.github.com>
2025-03-18 16:57:40 +0900

    Test: 2025-03-18 코딩 기초 트레이닝 문제풀이 (#44)

---
+++ 권수영/0318/181883_수열과 구간 쿼리 1.js
@@ -0,0 +1,46 @@
+/**
+ * 문제 설명
+ * 정수 배열 arr와 2차원 정수 배열 queries이 주어집니다.
+ * queries의 원소는 각각 하나의 query를 나타내며, [s, e] 꼴입니다.
+ * 각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 arr[i]에 1을 더합니다.
+ * 위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
+ * 
+ * 제한사항
+ * 1 ≤ arr의 길이 ≤ 1,000
+ * 0 ≤ arr의 원소 ≤ 1,000,000
+ * 1 ≤ queries의 길이 ≤ 1,000
+ * 0 ≤ s ≤ e < arr의 길이
+ */
+
+function solution(arr, queries) {
+  queries.forEach(query => {
+    const [s, e] = query;
+    for (let i = s; i <= e; i++) {
+      arr[i] += 1
+    }
+  });
+  return arr
+}
+
+/**
+ * 다른 풀이 1 (while 사용)
+function solution(arr, queries) {
+    queries.forEach(([s, e]) => {
+        while (s <= e) arr[s++]++;
+    });
+    return arr;
+}
+
+ * 다른 풀이 2 (.map 사용)
+function solution(arr, q) {
+    var answer = [];
+
+    q.map((a)=>{
+        for(let i = a[0]; i<=a[1]; i++){
+            arr[i]+=1;
+        }
+    })
+
+    return arr;
+}
+ */
