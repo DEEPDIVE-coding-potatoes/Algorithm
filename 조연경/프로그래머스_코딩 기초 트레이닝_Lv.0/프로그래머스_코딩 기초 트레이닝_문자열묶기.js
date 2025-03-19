3082da266ae34358c1ee7d595e0e2478f3c5db16
조연경 <sig.yg5057@gmail.com>
2025-03-19 16:16:32 +0900

    Test: 25-03-19 조연경 알고리즘 문제 풀이

---
+++ 조연경/프로그래머스_코딩 기초 트레이닝_Lv.0/프로그래머스_코딩 기초 트레이닝_문자열묶기.js
@@ -0,0 +1,26 @@
+// 문제 설명
+// 문자열 배열 strArr이 주어집니다. strArr의 원소들을 길이가 같은 문자열들끼리 그룹으로 묶었을 때 가장 개수가 많은 그룹의 크기를 return 하는 solution 함수를 완성해 주세요.
+
+// 제한사항
+// 1 ≤ strArr의 길이 ≤ 100,000
+// 1 ≤ strArr의 원소의 길이 ≤ 30
+// strArr의 원소들은 알파벳 소문자로 이루어진 문자열입니다.
+
+
+
+function solution(strArr) {
+  const lengthStr = strArr.reduce((acc, str) => {
+    acc[str.length] = (acc[str.length] || 0) + 1;
+    return acc;
+  }, {});
+
+  let MAX = 0;
+
+  for (let key in lengthStr) {
+    if (lengthStr[key] > MAX) {
+      MAX = lengthStr[key];
+    }
+  }
+
+  return MAX;
+}
